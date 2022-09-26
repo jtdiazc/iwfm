6c2bf86aed55c9c60f2dfe12e3e5e506db446b66
Jose Diaz <jdiaz@hydrofocus.com>
2022-09-26 14:42:52 -0700

    Adds column with dates in Pandas format

--- iwfm/read_sim_hyds_df.py
+++ iwfm/read_sim_hyds_df.py
@@ -101,5 +101,7 @@ def read_sim_hyds_df(nhyds, gwhyd_files):
 
             lines_i += 1
 
+    #Let's convert dates to Pandas date format
+    hyd_df["Date"] = pd.to_datetime(gwhyd_sim["TIME"].str[:-6], format="%m/%d/%Y")
 
     return hyd_df
