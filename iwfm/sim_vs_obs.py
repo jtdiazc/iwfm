cb42eff8163a3bd85f1da929d9a258cce6270548
Jose Diaz <jdiaz@hydrofocus.com>
2022-09-30 15:58:32 -0700

    Path of module

--- iwfm/sim_vs_obs.py
+++ iwfm/sim_vs_obs.py
@@ -17,32 +17,22 @@
 # -----------------------------------------------------------------------------
 
 import pandas as pd
-import sys
-sys.path.insert(0, r'P:\Projects\5658_NSJWCD\IWRFM\iwfm')
-#Let's import iwfm
 import iwfm
 
 
 
-def match_obs_sim(OBS,
-                  gwhyd_sim,
-                  keys=["Date",
-                        "Name",
-                        "Month",
-                        "Year"]):
-    ''' match_obs_sim - matches observations and simulated values for the same month and year
+
+def sim_vs_obs(OBS,gwhyd_sim):
+    ''' sim_vs_obs() - draws a scatterplot of observed vs simulated values
 
     Parameters
     ----------
     OBS: Pandas dataframe with observations. Date in "Date" column
     gwhyd_sim: Pandas dataframe with simulated time series (output of read_sim_hyds_df)
-    keys: columns to match when joining dataframes
 
 
     Returns
     -------
-
-    OBS_SIM: Pandas dataframe with OBS and SIM for joined rows
 
 
 
@@ -50,18 +40,9 @@ def match_obs_sim(OBS,
 
     '''
 
-    #First, lets add columns with year and month values
-
-    OBS["Year"]=OBS.Date.dt.year
-    OBS["Month"] = OBS.Date.dt.month
+    #First, let's match obs and sim records for same well, month, and year
+    iwfm.match_obs_sim(OBS,gwhyd_sim)
 
 
-    gwhyd_sim["Year"]=gwhyd_sim.Date.dt.year
-    gwhyd_sim["Month"] = gwhyd_sim.Date.dt.month
-
-    OBS_SIM=pd.merge(OBS, gwhyd_sim, on=keys, how='inner')
-
-
-
-    return OBS_SIM
+    return
 
