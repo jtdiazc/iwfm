e177708d77c9f3c9a85f3b2d7e338b9d16bdb1e9
Jose Diaz <jdiaz@hydrofocus.com>
2022-10-06 17:24:45 -0700

    Fixed some bugs

--- iwfm/CASGEM_hyds.py
+++ iwfm/CASGEM_hyds.py
@@ -131,40 +131,52 @@ def CASGEM_hyds(gwe_path,wells_df,gwhyd_sim,dir_out,sim_period,y_range,stations_
     wells_df["IOUTHL"]=wells_df["IOUTHL"].astype(int)
     wells_df["HYDROGRAPH ID"] = wells_df["HYDROGRAPH ID"].astype(int)
 
-
+    IWFM_in_CASGEM=IWFM_in_CASGEM.unique()
 
     #Let's loop through wells for which we have both simulations and observations
     for well in IWFM_in_CASGEM:
         #Let's grab well information
-        station_dum=stations[(stations.WELL_NAME==well)|(stations.SWN==well)].reset_index(drop=True)
+        station_dum=stations[(stations.WELL_NAME==well)|(stations.SWN==well)|(stations.WELL_NAME.str.find(well)>=0)].reset_index(drop=True)
         screen_top_dum=station_dum.loc[0,'GSE'] - station_dum.loc[0,'TOP_PRF']
         screen_bot_dum = station_dum.loc[0, 'GSE'] - station_dum.loc[0, 'BOT_PRF']
+        screen_length_dum=screen_top_dum-screen_bot_dum
         well_bot_dum=station_dum.loc[0, 'GSE'] - station_dum.loc[0, 'WELL_DEPTH']
+        well_depth_dum=station_dum.loc[0, 'WELL_DEPTH']
         wells_dum=wells_df[wells_df.Name == well].reset_index(drop=True)
         wells_df.loc[wells_df.Name == well,"Screen_top"]=screen_top_dum
         wells_df.loc[wells_df.Name == well, "Screen_bot"] = screen_bot_dum
         gwl_dum=gwl[(gwl.SWN==well)|(gwl.WELL_NAME==well)]
-        gwl_dum[(gwl_dum.Date >= sim_dates.loc[0, "Date"]) & (gwl_dum.Date <= sim_dates.loc[1, "Date"])].reset_index(
-            drop=True)
+        gwl_dum=gwl_dum[(gwl_dum.Date >= sim_dates.loc[0, "Date"]) & (gwl_dum.Date <= sim_dates.loc[1, "Date"])].reset_index(drop=True)
         top_dum=wells_dum.Top.unique()[0]
         #Let's retrieve which hydrographs we will need
         IDs_dum=[]
+        #List with weights
+        w=[]
         #Do we have nan in screen values?
         if (np.isnan(screen_top_dum)|np.isnan(screen_bot_dum)):
             #In this case, we add all the layers included within the well depth
             for i in range(nlay):
+                #Bottom of the layer
+                bot_lay_dum = wells_dum.loc[0, "L" + str(i + 1) + "_bot"]
                 #If the top of the layer is above the bottom of the well, we will add the layer to the list
                 #Layer 1
                 if i==0:
                     #Surface elevation
                     top_lay_dum=wells_dum.loc[0,'Top']
-                    if (top_lay_dum>well_bot_dum):
-                        IDs_dum.append(wells_dum.loc[wells_dum.IOUTHL == i + 1, "HYDROGRAPH ID"].values[0])
+                    IDs_dum.append(wells_dum.loc[wells_dum.IOUTHL == i + 1, "HYDROGRAPH ID"].values[0])
+                    #Let's add weight
+                    #Length of layer in well
+                    lay_length=min(top_dum-bot_lay_dum,top_dum-well_bot_dum)
+                    w.append(lay_length)
                 else:
                     # Top of layer
                     top_lay_dum = wells_dum.loc[0, "L" + str(i) + "_bot"]
                     if (top_lay_dum>well_bot_dum):
                         IDs_dum.append(wells_dum.loc[wells_dum.IOUTHL == i + 1, "HYDROGRAPH ID"].values[0])
+                        lay_length = min(top_lay_dum - bot_lay_dum, top_lay_dum - well_bot_dum)
+                        w.append(lay_length)
+            # Let's normalize weights
+            w = w / well_depth_dum
 
 
         else:
@@ -177,6 +189,9 @@ def CASGEM_hyds(gwe_path,wells_df,gwhyd_sim,dir_out,sim_period,y_range,stations_
                 if i==0:
                     if ~(screen_top_dum<bot_lay_dum):
                         IDs_dum.append(wells_dum.loc[wells_dum.IOUTHL==1,"HYDROGRAPH ID"].values[0])
+                        #Let's add weight
+                        lay_length = min(screen_top_dum - bot_lay_dum, top_dum - bot_lay_dum,screen_top_dum - screen_bot_dum)
+                        w.append(lay_length)
                 #For the rest of the layers
                 else:
                     #Top of layer
@@ -185,6 +200,14 @@ def CASGEM_hyds(gwe_path,wells_df,gwhyd_sim,dir_out,sim_period,y_range,stations_
                     bot_lay_dum=wells_dum.loc[0, "L" + str(i+1) + "_bot"]
                     if~((screen_top_dum<bot_lay_dum)|(screen_bot_dum>top_lay_dum)):
                         IDs_dum.append(wells_dum.loc[wells_dum.IOUTHL == i+1, "HYDROGRAPH ID"].values[0])
+                        #Let's add weight
+                        lay_length = min(screen_top_dum - bot_lay_dum, top_lay_dum - bot_lay_dum,screen_top_dum - screen_bot_dum)
+                        w.append(lay_length)
+
+            w = w / screen_length_dum
+
+
+
 
 
         #Now, let's grab simulations for the well
@@ -194,7 +217,7 @@ def CASGEM_hyds(gwe_path,wells_df,gwhyd_sim,dir_out,sim_period,y_range,stations_
         sim_dum=gwhyd_sim.loc[gwhyd_sim['HYDROGRAPH ID'].isin(IDs_dum),['Date','HYDROGRAPH ID', 'LAYER','SIM']].reset_index(drop=True)
 
         #Let's transform to wide
-        obs_dum_wide=sim_dum.pivot(index="Date",columns='LAYER',values="SIM")
+        sim_dum_wide=sim_dum.pivot(index="Date",columns='LAYER',values="SIM")
 
         #Names for wide dataframe
         wide_names=[]
@@ -203,36 +226,78 @@ def CASGEM_hyds(gwe_path,wells_df,gwhyd_sim,dir_out,sim_period,y_range,stations_
             lay_dum=wells_dum.loc[wells_dum["HYDROGRAPH ID"]==IDs_dum[i],'IOUTHL'].values[0]
             wide_names.append("Layer_"+str(lay_dum))
 
+        sim_dum_wide.columns = wide_names
+        sim_dum_wide["Date"]=sim_dum_wide.index
 
-        #Let's plot hydrographs
+        sim_dum_wide=sim_dum_wide[["Date"] + wide_names].reset_index(drop=True)
+
+        #Let's calculate weighted average
+        avg_w=sim_dum_wide.iloc[:,1]*0
+        for i in range(len(IDs_dum)):
+            avg_w=avg_w+(sim_dum_wide['Layer_'+str(i+1)]*w[i])
+        sim_dum_wide["Avg_w"]=avg_w
+
+        #Let's prepare dates to join dataframes
+
+        sim_dum_wide.loc[:,"Year_Month"]=pd.to_datetime(sim_dum_wide.Date.dt.strftime('%Y/%m'))
+
+        gwl_dum.loc[:,"Year_Month"]=pd.to_datetime(gwl_dum.Date.dt.strftime('%Y/%m'))
+
+        all_wide=pd.merge(sim_dum_wide, gwl_dum[["Year_Month",'WSE']], on="Year_Month", how='left')
+
+        #Let's export to csv
+        all_wide.to_csv(os.path.join(dir_out,well+".csv"))
+
+        #Let's plot observations
+
         ax = gwl_dum.plot(x='Date',y='WSE',marker='o',linestyle = 'None',title=well)
-        obs_dum.plot(ax=ax, x='Date', y='SIM')
-        y_lb=int((gwl_dum.WSE.mean()-y_range/2)/5)*5
-        y_ub=y_lb+y_range
+        #Let's plot the rest of the series
+        for i in range(len(IDs_dum)):
+            sim_dum_wide.plot(ax=ax, x='Date', y='Layer_'+str(i+1))
+
+        #If there is more than one well, we add weighted average
+        if len(IDs_dum)>1:
+            sim_dum_wide.plot(ax=ax, x='Date', y='Avg_w')
+
+        #obs_dum.plot(ax=ax, x='Date', y='SIM')
+        #y_lb=int((gwl_dum.WSE.mean()-y_range/2)/5)*5
+        #y_ub=y_lb+y_range
+        y_lb=-75
+        y_ub=75
         ax.set_ylim(y_lb,y_ub)
+        #Let's set tickmarks
+        axes=ax.axes
+        year_0=sim_dates.Date.dt.year[0]
+        year_f = sim_dates.Date.dt.year[1]
+        xticks_dum=pd.date_range(start=sim_dates.Date[0], end=sim_dates.Date[1], freq='Y')
+        xlabs_dum=xticks_dum.strftime('%Y')
+        yticks_dum=list(range(-75,100,25))
+        ax.axes.set_xticks(xticks_dum)
+        ax.axes.set_yticks(yticks_dum)
+        ax.axes.set_xticklabels(xlabs_dum)
         fig = ax.get_figure()
         fig.savefig(os.path.join(dir_out,well+".png"))
         fig.clear()
 
-        ranges[well]=gwl_dum.WSE.max()-gwl_dum.WSE.min()
+        #ranges[well]=gwl_dum.WSE.max()-gwl_dum.WSE.min()
 
         #Boxplot
         #now, we have to merge both dataframes into one
-        gwl_dum=gwl_dum[['Date','WSE']]
-        gwl_dum["Type"]='WSE'
-        gwl_dum=gwl_dum.rename(columns={'WSE':'head'})
+        #gwl_dum=gwl_dum[['Date','WSE']]
+        #gwl_dum["Type"]='WSE'
+        #gwl_dum=gwl_dum.rename(columns={'WSE':'head'})
 
-        obs_dum=obs_dum[["Date", "SIM"]]
-        obs_dum["Type"] = 'SIM'
-        obs_dum = obs_dum.rename(columns={"SIM": 'head'})
+        #obs_dum=obs_dum[["Date", "SIM"]]
+        #obs_dum["Type"] = 'SIM'
+        #obs_dum = obs_dum.rename(columns={"SIM": 'head'})
 
-        obs_dum = obs_dum.append(gwl_dum)
+        #obs_dum = obs_dum.append(gwl_dum)
 
-        ax2=obs_dum.boxplot(by="Type",column='head')
-        plt.title(well)
-        fig2 = ax2.get_figure()
-        fig2.savefig(os.path.join(dir_out, well + "_boxplot.png"))
-        fig2.clear
+        #ax2=obs_dum.boxplot(by="Type",column='head')
+        #plt.title(well)
+        #fig2 = ax2.get_figure()
+        #fig2.savefig(os.path.join(dir_out, well + "_boxplot.png"))
+        #fig2.clear
 
     #Let's subset now all the observations that will be useful
     OBS=gwl[gwl.WELL_NAME.isin(IWFM_in_CASGEM)|gwl.SWN.isin(IWFM_in_CASGEM)].copy().reset_index(drop=True)
